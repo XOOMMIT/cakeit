b028099e5c69df3aa50e22169c4b4f72de26b128
Mike van Eckendonk <mike.e@vecks.com.br>
2022-07-29 06:16:58 -0300

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Seo from "../components/SEO"
+
+const IndexPage = () => (
+  <div>
+    <Seo title="Home" />
+    <p>Start here!</p>
+  </div>
+)
+
+export default IndexPage
